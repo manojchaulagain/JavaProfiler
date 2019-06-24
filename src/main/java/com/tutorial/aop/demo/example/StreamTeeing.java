279a4c3e98cad57ca59c5644c0aceeb23ec7ab45
Manoj Chaulagain <machaulagain@sbgtv.com>
2019-06-24 15:14:18 -0700

    Fixed the nameing convention

---
+++ src/main/java/com/tutorial/aop/demo/example/StreamTeeing.java
@@ -0,0 +1,28 @@
+package com.tutorial.aop.demo.example;
+
+import java.util.stream.Stream;
+
+import static java.util.stream.Collectors.counting;
+import static java.util.stream.Collectors.summingDouble;
+import static java.util.stream.Collectors.summingInt;
+import static java.util.stream.Collectors.teeing;
+
+public class StreamTeeing {
+
+    public static void main( String[] args ) {
+        calculateAverage();
+    }
+
+    private static void calculateAverage() {
+
+        double average = Stream.of( 1, 4, 2, 7, 5, 5, 3 ).collect( teeing( summingDouble( i -> i ), counting(), ( sum, n ) -> sum / n ) );
+        System.out.println( average );
+
+        long a = Stream.of( "String 1", "asdf", "asdf" ).collect( teeing( summingInt( String::length ), counting(), ( sum, i ) -> sum / i ) );
+        System.out.println( a );
+
+        String test = " asdfasdf";
+
+        System.out.println(test.transform( x -> x.substring( 3 ) ) + "");
+    }
+}
---
+++ src/main/java/com/tutorial/aop/demo/example/SwitchTrial.java
@@ -0,0 +1,20 @@
+package com.tutorial.aop.demo.example;
+
+public class SwitchTrial {
+
+    enum Day {
+        SUNDAY, MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY, SATURDAY
+    }
+
+    public static void main( String[] args ) {
+        testSwitch( Day.WEDNESDAY );
+        testSwitch( Day.MONDAY );
+    }
+
+    private static void testSwitch( Day day ) {
+        switch ( day ) {
+            case SUNDAY, MONDAY, TUESDAY -> System.out.println( "Testing Switch " + day.name() );
+            case WEDNESDAY, THURSDAY, FRIDAY, SATURDAY -> System.out.println( "Testing Switch Again " + day.name() );
+        }
+    }
+}
